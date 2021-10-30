7c348e76ce08b0f1d0617cef2a3845eca5fac1ed
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-30 00:33:49 -0700

    Sun Rui 19970309    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: faabd5867fd04f18a2520c1d835f54f0            Boot ID: d18b069696334a18b3e5b4d612167899     Virtualization: vmware   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  00:33:49 up  7:24,  1 user,  load average: 0.41, 0.61, 0.78

--- projects/cpu/vsrc/if_stage.sv
+++ projects/cpu/vsrc/if_stage.sv
@@ -8,34 +8,30 @@ module if_stage(
   input                rst,
   input [ `XLEN-1 : 0] inst,
   input                stall,
-  input [      63 : 0] target_PC,
+  input [     63 : 0 ] target_PC,
   
   output logic [63 : 0] inst_addr,
-  output logic          inst_ena,
-  output IF_ID_PACKET   if_packet_out
-
+  output logic         inst_ena
+  
 );
 
-logic [63 : 0] PC, NPC;
+logic [63 : 0] pc;
 
 // fetch an instruction
 always_ff @( posedge clk )
 begin
-  if( rst == 1'b1 ) begin
-    PC <= `ZERO_WORD ;
-    inst_ena <= 1'b0;
+  if( rst == 1'b1 )
+  begin
+    pc <= `ZERO_WORD ;
   end
-  else begin 
-    PC <= NPC;
-    inst_ena <= 1'b1;
+  else
+  begin
+    pc <= pc + 4;
   end
 end
 
-assign inst_addr = PC;
-assign NPC = PC + 4;
-assign if_packet_out.inst = inst;
-assign if_packet_out.valid = 1;
-assign if_packet_out.NPC = NPC;
-assign if_packet_out.PC = PC;
+assign inst_addr = pc;
+assign inst_ena  = ( rst == 1'b1 ) ? 0 : 1;
+
 
 endmodule
